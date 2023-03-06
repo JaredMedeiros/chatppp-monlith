c564e7caab98811d6fd29e10202a8bc8e880ea54
JaredMedeiros <97901520+JaredMedeiros@users.noreply.github.com>
2023-03-05 22:15:29 -0600

    set up API and designed message system to communicate with backend

--- chatppp/index.js
+++ chatppp/index.js
@@ -1,29 +1,36 @@
-const express = require('express')
-
-
 const { Configuration, OpenAIApi } = require( "openai");
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+
+
+
+
 const configuration = new Configuration({
     organization: "org-PjMrGQ5CV5Wv4yazLkkICyNT",
     apiKey: process.env.OPENAI_API_KEY,
 });
+
 const openai = new OpenAIApi(configuration);
 
-
-//create a simple express api that calls the function from above
 const app = express()
+app.use(bodyParser.json())
+app.use(cors())
+
 const port = 3500
 
 app.post( '/', async (req, res) => {
+    const { message } = req.body;
     const response = await openai.createCompletion({
         model: "text-davinci-003",
-        prompt: "Say this is a test",
-        temperature: 0,
+        prompt: `${message}`,
+        max_tokens: 100,
+        temperature: 0.5,
     });
-    console.log(response.data.choices[0].text)
     res.json({
-        data: response.data
+        message: response.data.choices[0].text,
     })
-})
+});
 
 app.listen(port, () => {
     console.log(`listening at http://localhost:${port}`)
